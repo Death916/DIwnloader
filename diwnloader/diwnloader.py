d715c6679066336a3deef9668ded46a008dfbca0
death916 <tavn1992@gmail.com>
2021-04-12 01:27:38 -0700

    login

--- diwnloader/diwnloader.py
+++ diwnloader/diwnloader.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import requests
+import os
+import json
+import selenium
 
 driver = webdriver.Edge()
 mainPage = "https://di.fm/login"
 driver.get(mainPage)
 username = "tavn1992@gmail.com"
 login_page = "http://di.fm/login"
-session_requests = requests.session()
+
+print(os.getcwd())
+
+with open(os.getcwd() + "\diwnloader"  + "\login.json") as k:
+    keys = json.load(k)
 
 
 
-driver.find_element_by_name('member_session[username]').send_keys(payload[0])
+driver.find_element_by_name('member_session[username]').send_keys(keys['username'])
+driver.find_element_by_name('member_session[password]').send_keys(keys['password'])
 
+driver.find_element_by_class_name('btn.submit-btn').click()
