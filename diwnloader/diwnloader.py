3d82f4a7c350be50ae5e428955e8c1dac15ab50e
death916 <tavn1992@gmail.com>
2021-05-05 03:00:43 -0700

    login and change to likes

--- diwnloader/diwnloader.py
+++ diwnloader/diwnloader.py
@@ -5,20 +5,30 @@ import os
 import json
 import selenium
 
-driver = webdriver.Edge()
-mainPage = "https://di.fm/login"
-driver.get(mainPage)
-username = "tavn1992@gmail.com"
-login_page = "http://di.fm/login"
-
-print(os.getcwd())
-
-with open(os.getcwd() + "\diwnloader"  + "\login.json") as k:
-    keys = json.load(k)
+def main():
+    
+    driver = webdriver.Edge()
+    mainPage = "https://di.fm/login"
+    driver.get(mainPage)
+    username = "tavn1992@gmail.com"
 
 
+    print(os.getcwd())
 
-driver.find_element_by_name('member_session[username]').send_keys(keys['username'])
-driver.find_element_by_name('member_session[password]').send_keys(keys['password'])
+    def login():
+        with open(os.getcwd() + "\diwnloader"  + "\login.json") as k:
+            keys = json.load(k)
 
-driver.find_element_by_class_name('btn.submit-btn').click()
+        driver.find_element_by_name('member_session[username]').send_keys(keys['username'])
+
+        driver.find_element_by_name('member_session[password]').send_keys(keys['password'])
+
+        driver.find_element_by_class_name('btn.submit-btn').click()
+
+
+    connect()
+
+    def get_likes():
+        likes = 'https://www.di.fm/my/likes'
+
+        driver.get(likes)
